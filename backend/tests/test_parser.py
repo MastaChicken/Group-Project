0ac1b4e1687c18b3146d5db3a54566207d7587b2
Wil <psyws2@nottingham.ac.uk>
2021-11-29 16:28:25 +0000

    Add docstrings to test_parser

--- backend/tests/test_parser.py
+++ backend/tests/test_parser.py
@@ -1,7 +1,9 @@
+"""Tests the metadata from the pdf."""
+from typing import Any
 from app.parser import Parser
 import fitz
 
-from datetime import datetime, timezone
+from datetime import datetime
 
 new_pdf = fitz.open()
 
@@ -9,7 +11,9 @@ pdf_title = "Test PDF"
 pdf_author = "Wil"
 pdf_time = fitz.get_pdf_now()
 
-new_pdf.set_metadata({"title": pdf_title, "author": pdf_author, "creationDate": pdf_time})
+new_pdf.set_metadata(
+    {"title": pdf_title, "author": pdf_author, "creationDate": pdf_time}
+)
 
 now_time = float(int(datetime.timestamp(datetime.now())))
 
@@ -19,14 +23,20 @@ new_pdf.save("test.pdf")
 
 doc = Parser("test.pdf")
 
-def test_pdf_title():
+
+def test_pdf_title() -> Any:
+    """Test pdf title."""
     title = doc.metadata["title"]
     assert title == pdf_title
 
-def test_pdf_authors():
+
+def test_pdf_authors() -> Any:
+    """Test pdf author."""
     author = doc.metadata["author"]
     assert author == pdf_author
 
-def test_pdf_timestamp():
-    cTime = doc.metadata["creationTimestamp"]
-    assert cTime == now_time
+
+def test_pdf_timestamp() -> Any:
+    """Test pdf timestamp."""
+    c_time = doc.metadata["creationTimestamp"]
+    assert c_time == now_time
