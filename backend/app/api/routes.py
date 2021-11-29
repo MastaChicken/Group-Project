9694c8a0b5b25c0ca453275934c6537a2aec6b9e
Sean Butcher <psysb11@exmail.nottinhgam.ac.uk>
2021-11-29 21:53:02 +0000

    Format and lint routes.py

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,3 +1,10 @@
+"""Contains endpoint for the API.
+
+Todo:
+    * add check for size error in recieve_file
+    * add more endpoints
+"""
+
 import requests
 from app.parser import Parser
 from fastapi import APIRouter, File, HTTPException, UploadFile, status
@@ -7,7 +14,16 @@ router = APIRouter()
 
 @router.post("/upload")
 async def recieve_file(file: UploadFile = File(...)):
-    # TODO: test with large pdfs?
+    """Recieves uploaded file and sets it to object.
+
+    Args:
+        file: file which is uploaded
+    Returns:
+        JSON file describing text, table of contents and metadata
+    Raises:
+        SizeError: if given file is too large
+    """
+    # TODO: test with large pdfs? add to docstring
     contents = await file.read()
     test = Parser(contents)
     return {
@@ -19,6 +35,17 @@ async def recieve_file(file: UploadFile = File(...)):
 
 @router.get("/test/")
 def valid_pdf_url(url: str):
+    """Checks to see if URL of a PDF is valid.
+
+    Args:
+        url: url of the file to be validated
+    Returns:
+        JSON file determining saying file is valid
+    Raises:
+        RequestError: unable to request pdf from given url
+        FileError: given url does not contain a pdf file
+        FormatError: url is invalid link
+    """
     try:
         res = requests.head(url)
         if res.status_code != 200:
