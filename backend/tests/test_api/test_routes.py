fe75bb49ee1e840b33a34e4ddc9c5a3e2bbef814
Sean Butcher <psysb11@exmail.nottinhgam.ac.uk>
2021-11-30 13:15:54 +0000

    Add invalid tests to validate_url endpoint

--- backend/tests/test_api/test_routes.py
+++ backend/tests/test_api/test_routes.py
@@ -1,15 +1,12 @@
 """Unit tests for API routes.
 
 Todo:
-    
+    * added more test cases for all
 """
 from typing import Any
-from fastapi.exceptions import HTTPException
-from requests.utils import quote
 from app.main import app
 from app.parser import ParserModel
 from fastapi.testclient import TestClient
-import pytest
 
 client = TestClient(app)
 
@@ -47,9 +44,29 @@ class TestValidateURL:
 
     def test_valid_request(self) -> Any:
         """Request should return 200/0K and valid json response."""
-
         response = client.get(
             "/validate_url/", params={"url": "https://www.orimi.com/pdf-test.pdf"}
         )
         assert response.status_code == 200
         assert response.json()["detail"] == "PDF URL is valid"
+
+    def test_invalid_url(self) -> Any:
+        """Request should return 500 and invalid json response."""
+        response = client.get("/validate_url/", params={"url": "invalidlink"})
+        assert response.status_code == 500
+        assert response.json()["detail"] == "Parameter has an invalid format"
+
+    def test_invalid_request(self) -> Any:
+        """Request shouldn't return 200 status code and unsuccessful json."""
+        response = client.get(
+            "/validate_url/",
+            params={"url": "https://www.businessinsider.com/techreference"},
+        )
+        assert response.status_code != 200
+        assert response.json()["detail"] == "Request unsuccessful"
+
+    def test_invalid_pdf(self) -> Any:
+        """Request should return 415 and unsupported json."""
+        response = client.get("/validate_url/", params={"url": "http://www.google.com"})
+        assert response.status_code == 415
+        assert response.json()["detail"] == "File has unsupported extension type"
