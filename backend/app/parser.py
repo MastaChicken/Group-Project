82c42640bab7f0747889ca93b2ab8ad4d6475f1c
Omar Zeghouani <psyoz1@nottingham.ac.uk>
2021-12-07 12:38:31 +0000

    Implement title extraction
    - Change the text property return value
    
    - Add font_sizes as property (requires refactoring)

--- backend/app/parser.py
+++ backend/app/parser.py
@@ -15,8 +15,11 @@ Todo:
 
 from __future__ import annotations
 
+from collections import defaultdict
 from datetime import datetime, timezone
+from difflib import SequenceMatcher
 from functools import cached_property
+from statistics import fmean, pvariance
 
 import fitz
 from pydantic.main import BaseModel
@@ -26,10 +29,13 @@ class ParserModel(BaseModel):
     """Object used as a response model.
 
     Models public properties in Parser
+
+    Todo:
+        * Add new properties to model
     """
 
     metadata: dict
-    text: str
+    text: list[dict]
     toc: list
 
 
@@ -106,16 +112,16 @@ class Parser:
         return metadata
 
     @cached_property
-    def text(self) -> str:
+    def text(self) -> list[dict]:
         """Document text.
 
         Returns:
             Text content from entire document
 
         """
-        text: str = ""
+        text: list[dict] = []
         for page in self._doc:
-            text += str(page.get_text("text", flags=fitz.TEXT_DEHYPHENATE))
+            text.append(page.get_text("dict", sort=False, flags=fitz.TEXT_DEHYPHENATE))
 
         return text
 
@@ -128,3 +134,70 @@ class Parser:
 
         """
         return self._doc.get_toc()
+
+    @cached_property
+    def title(self) -> str:
+        """Document title.
+
+        Iterates around the first page, concatanating text chunks with large font sizes.
+        If a title is in the PDF metadata, use a similarity ratio to check whether to
+        use parsed_title or extracted_title.
+
+        Returns:
+            Title extracted from metadata/first page
+
+        Notes:
+            title_chunks should never be empty. Ensure that the page is not empty
+            (add test).
+        """
+        title_chunks: dict[float, str] = defaultdict(str)
+        max_font_size = fmean(self.font_sizes) + 2 * pvariance(self.font_sizes)
+
+        # Iterating around first page blocks
+        for block in self.text[0]["blocks"]:
+            for line in block["lines"]:
+                for span in line["spans"]:
+                    text_size = span["size"]
+                    if text_size > max_font_size:
+                        title_chunks[text_size] += span["text"] + " "
+
+        parsed_title = title_chunks[max(title_chunks.keys())]
+        extracted_title = self.metadata["title"]
+
+        if not extracted_title:
+            return parsed_title.strip()
+
+        # NOTE: should we trust that the pdf metadata title is correct?
+        for value in title_chunks.values():
+            similarity = SequenceMatcher(None, extracted_title, value).quick_ratio()
+            if round(similarity, 1) == 1:
+                parsed_title = extracted_title
+                break
+
+        return parsed_title.strip()
+
+    @cached_property
+    def font_sizes(self) -> list[float]:
+        # TODO: refactor this function
+        # it should be more generic
+        font_sizes = []
+
+        for page in self.text:
+            for block in page["blocks"]:
+                for line in block["lines"]:
+                    for span in line["spans"]:
+                        font_sizes.append(span["size"])
+
+        return font_sizes
+
+
+if __name__ == "__main__":
+    with (
+        open("samples/sampleScholar.pdf", "rb") as pdf,
+        Parser(pdf.read()) as test,
+    ):
+        from pprint import pprint
+
+        pprint(test.text[0]["blocks"][0])
+        print(test.metadata)
+        print(test.title)
