1977b2508ba8cd529c3baa19461a8829a9e124c1
Omar Zeghouani <psyoz1@nottingham.ac.uk>
2021-12-14 19:19:19 +0000

    Turn techniques module into class

--- backend/app/nlp/techniques.py
+++ backend/app/nlp/techniques.py
@@ -1,88 +1,115 @@
-"""Contains the techniques used within the nlp process."""
+"""Contains the NLP techiniques that can be applied onto text chunks.
+
+Example::
+
+    text: str
+    nlp = Techniques(text)
+    nlp.extractive_summarisation(5)
+
+Todo:
+    * Add more techiniques
+"""
 from collections import Counter
-from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
+from functools import cached_property
 from heapq import nlargest
+
+from spacy import load
+from spacy.lang.en.stop_words import STOP_WORDS
 from spacy.tokens.doc import Doc
+from spacy.tokens.span import Span
 
 
-# adds the newline token to the punctuation array
-punctuation += "\n"
-stopwords = list(STOP_WORDS)
+class Techniques:
+    """Apply NLP techiniques on text.
 
-
-def word_freq(doc: Doc) -> dict:
-    """Calculates the frequencies of word in a given document.
-
-    Args:
-        doc: document containing the text to work the frequencies of
-
-    Returns:
-        dictionary containing the frequency of each word
+    Attributes:
+        __doc: spaCy Doc class
     """
-    word_frequencies = {}
-    for word in doc:
-        if (
-            word.text.lower() not in stopwords
-            and word.text.lower() not in punctuation
-            and word.pos_ == "NOUN"
-        ):
-            if word.text not in word_frequencies.keys():
-                word_frequencies[word.text] = 1
-            else:
-                word_frequencies[word.text] += 1
 
-    return word_frequencies
+    __doc: Doc
 
+    def __init__(self, text: str):
+        """Run English spacy model on text chunk.
 
-def extractive_summarisation(doc: Doc, n: int) -> list[str]:
-    """Returns extractive summarisation of given document down to 'n' amount of sentences.
+        Args:
+            text : chunk of text from scholarly article
+        """
+        nlp = load("en_core_web_sm")
+        self.__doc = nlp(text)
 
-    Args:
-        doc: document containing text to be summarised
-        n : amount of sentences to be reduced down to
+    @cached_property
+    def word_freq(self) -> dict:
+        """Calculate the frequencies of word in a given document.
 
-    Returns:
-        list of strings containing the extractive summarisation
-    """
-    word_frequencies = word_freq(doc)
+        Returns:
+            Dictionary containing the frequency of each word
+        """
+        from string import punctuation
 
-    max_frequency = max(word_frequencies.values())
-    for word in word_frequencies.keys():
-        word_frequencies[word] = word_frequencies[word] / max_frequency
+        punctuation += "\n"
 
-    # need to optimise this, only counts sentence if fullstop, space then capital letter/punctuation/number
-    sentence_tokens = [sent for sent in doc.sents]
-
-    sentence_scores = {}
-    for sent in sentence_tokens:
-        for word in sent:
-            if word.text.lower() in word_frequencies.keys():
-                if sent not in sentence_scores.keys():
-                    sentence_scores[sent] = word_frequencies[word.text.lower()]
+        word_frequencies = {}
+        for word in self.__doc:
+            if (
+                word.text.lower() not in STOP_WORDS
+                and word.text.lower() not in punctuation
+                and word.pos_ == "NOUN"
+            ):
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
                 else:
-                    sentence_scores[sent] += word_frequencies[word.text.lower()]
+                    word_frequencies[word.text] += 1
 
-    summarised_sentences = nlargest(n, sentence_scores, key=sentence_scores.get)
+        return word_frequencies
 
-    final_sentences = [w.text for w in summarised_sentences]
-    return final_sentences
+    def extractive_summarisation(self, n: int) -> list[str]:
+        """Return summary down to 'n' amount of sentences.
 
+        Args:
+            n : amount of sentences to be reduced down to
 
-def top_common_n_words(doc: Doc, n: int) -> tuple[str, int]:
-    """Returns tuple containing most common n amount of words in given text.
+        Returns:
+            List of strings containing the extractive summarisation
+        """
+        word_frequencies = self.word_freq
 
-    Args:
-        doc (Doc): document with text to find most common n words
-        n (int): amount of common words to return
+        max_frequency = max(word_frequencies.values())
+        for word in word_frequencies.keys():
+            word_frequencies[word] = word_frequencies[word] / max_frequency
 
-    Returns:
-        tuple[str, int]: first element is the word, second element is the frequency
-    """
-    word_frequencies = word_freq(doc)
+        # need to optimise this, only counts sentence if fullstop,
+        # space then capital letter/punctuation/number
+        sentence_tokens = [sent for sent in self.__doc.sents]
 
-    # five most common noun tokens
-    noun_freq = Counter(word_frequencies)
-    common_nouns = noun_freq.most_common(n)
+        sentence_scores: dict[Span, float] = {}
+        for sent in sentence_tokens:
+            for word in sent:
+                if word.text.lower() in word_frequencies.keys():
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word.text.lower()]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word.text.lower()]
 
-    return common_nouns
+        summarised_sentences = nlargest(
+            n, sentence_scores, key=lambda k: sentence_scores[k]
+        )
+
+        final_sentences = [w.text for w in summarised_sentences]
+
+        return final_sentences
+
+    def top_common_n_words(self, n: int) -> list[tuple[str, int]]:
+        """Return tuple containing most common n amount of words in given text.
+
+        Args:
+            n : amount of common words to return
+
+        Returns:
+            List of n words including their frequency
+        """
+        word_frequencies = self.word_freq
+
+        noun_freq = Counter(word_frequencies)
+        common_nouns = noun_freq.most_common(n)
+
+        return common_nouns
