c89fa468ab82f9f64962804b8b15549525fce328
Omar Zeghouani <psyoz1@nottingham.ac.uk>
2022-04-11 15:05:55 +0100

    Refactor files for Vite

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
+import MLA8Citation from "./modules/MLA8Citation.js";
+
 const $ = (id) => document.getElementById(id);
 const API = "http://localhost:8000";
-let count = 0;
 
 /**
  * Checks if file is valid .pdf
@@ -24,58 +25,6 @@ function isValidPDF({ type, name }) {
 }
 
 /**
- * Takes the page count and runs checks to see whether the pages-to-summarise slider should be
- * displayed or not. If page count is equal to 1 then it is not shown as there is no range of pages
- * to be summarised. Else if it is over 1 then it shows the slider and updates the sliders attributes
- * to match the Page count. Finally it updates the label to reflect this.
- *
- * @param {*} c - Passes the count variable. This represents the Page count of the pdf file.
- */
-function displaySlider(c) {
-  const ptsSlider = $("pages-to-summarise");
-  count = c;
-  if (count == 1) {
-    $("selection-boxes").style.display = "none";
-  } else if (count > 1) {
-    $("selection-boxes").style.display = "block";
-    ptsSlider.max = count;
-    ptsSlider.value = count;
-    // Then it changes the text for pages to summarise to update with the current value
-    // and the max i.e
-    $(
-      "pts-lbl"
-    ).innerHTML = `Pages to Summarise: ${ptsSlider.value} / ${count}`;
-  }
-}
-
-/**
- * Creates a reader and reads the PDF document. When the file has been read it tries to set count
- * as result match on regex for /Type/Page[x]. If this is successful it calls displaySlider(), else
- * it catches the error, hides the slider and sets count to 0.
- *
- * This is inconsistent so far, as PDFs can be hidden behind multiple layers of obfuscation /
- * compression which affects the documents readability.
- *
- * @param {*} file - The PDF document stored in file input pdfpicker.
- */
-function setPagesToSummarise(file) {
-  // reads file and function triggers when reader has completed reading.
-  let reader = new FileReader();
-  reader.readAsBinaryString(file);
-  reader.onloadend = () => {
-    // let count = number of pages. sets the pages to summarise slider attribute max
-    // to the number of pages.
-    try {
-      let count = reader.result.match(/\/Type[\s]*\/Page[^s]/g).length;
-      displaySlider(count);
-    } catch (e) {
-      $("selection-boxes").style.display = "none";
-      count = 0;
-    }
-  };
-}
-
-/**
  * Adds event listeners for the pdfpicker file input, the pages to summarise slider, size of summary slider.
  *
  * The Event Listener for pdfpicker waits for change in the pdfpicker-file. Once detected this then checks
@@ -85,8 +34,10 @@ function setPagesToSummarise(file) {
  * Event for pages-to-summarise slider detects user changing value and updates label accordingly.
  *
  * Event for size-of-summary slider detects user changing value and updates label accordingly.
+ *
+ * Event for upload-form calls uploadPDF function on submit.
  */
-window.onload = function listenForFileUpload() {
+window.onload = () => {
   // Adds event listener to the upload button, so it executes on change.
   $("pdfpicker-file").addEventListener(
     "change",
@@ -95,7 +46,6 @@ window.onload = function listenForFileUpload() {
       let dropText = $("drop-text");
       // checks file exists and passes PDF checks.
       if (files.length > 0 && isValidPDF(files[0])) {
-        setPagesToSummarise(files[0]);
         dropText.innerHTML = `File accepted: ${files[0].name}`;
         $("selection-boxes").style.display = "block";
       } else {
@@ -108,17 +58,6 @@ window.onload = function listenForFileUpload() {
     false
   );
 
-  // adds event listener to the pages to summarise slider, and updates the label text on change
-  let ptsValue = $("pages-to-summarise");
-  ptsValue.addEventListener(
-    "change",
-    () => {
-      let max = ptsValue.getAttribute("max");
-      $("pts-lbl").innerHTML = `Pages to Summarise: ${ptsValue.value} / ${max}`;
-    },
-    true
-  );
-
   let sos = $("size-of-summary");
   sos.addEventListener(
     "change",
@@ -127,13 +66,36 @@ window.onload = function listenForFileUpload() {
     },
     true
   );
+
+  // Upload PDF form
+  let uploadForm = $("upload-form");
+  uploadForm.addEventListener("submit", (ev) => uploadPDF(ev));
+
+  // Upload URL
+  let urlInput = $("url-input");
+  urlInput.addEventListener("click", validateURL);
+
+  // Drophandler
+  let dropZone = $("drop-zone");
+  dropZone.addEventListener("drop", (ev) => dropHandler(ev));
+  dropZone.addEventListener("dragover", (ev) => dragOverHandler(ev));
+
+  // Toggle PDF
+  let pdfToggleInput = $("output-show-document");
+  // TODO: use event to check if its toggled or not
+  pdfToggleInput.addEventListener("change", togglePDFDisplay);
+
+  // Output display
+  let outputBoxes = document.querySelectorAll(".output-boxes");
+
+  outputBoxes.forEach((box) => box.addEventListener("click", toggleOpen));
 };
 
 /**
  * Function to monitor when files are dragged over the drag over box.
  * Important to remove browsers default functionality, i.e chrome wants to copy the pdf file into browser and render.
  *
- * @param {*} ev - dragOverHandler event.
+ * @param {DragEvent} ev - dragOverHandler event.
  */
 function dragOverHandler(ev) {
   console.log("File(s) in drop zone");
@@ -145,7 +107,7 @@ function dragOverHandler(ev) {
  * Function for handling drop events. Handles the file transfer from event.datatransfer file to being stored in the input="file".
  * clears dataTransfer data after stored, or determined not valid .pdf filetype.
  *
- * @param {*} ev - dropHandler event.
+ * @param {DragEvent} ev - dropHandler event.
  */
 function dropHandler(ev) {
   console.log("File(s) dropped");
@@ -163,7 +125,6 @@ function dropHandler(ev) {
         // If file is .pdf adjust the page summary slider and set the file input box as the data files then clear event data.
         // This ensures we are only dealing with 1 file at a time and from the same source, so when sending to backend
         // we only have to target the input file.
-        setPagesToSummarise(file);
         dropText.innerHTML = `File accepted: ${file.name}`;
         $("pdfpicker-file").files = ev.dataTransfer.files;
         clearData(ev.dataTransfer);
@@ -200,6 +161,7 @@ function clearData(data) {
  * @param {*} tabName - Desired tab to switch to.
  * @param {*} className - Class Name for all involved divs. (Think i included this to make it reuseable but may be able to remove this.)
  */
+// TODO: Replace tab functionality
 function openTab(tabName, className) {
   /*
   Couldn't get this ES6 version of the code to work properly, so i think something is wrong with it.
@@ -219,9 +181,6 @@ function openTab(tabName, className) {
   var show = document.getElementsByClassName(tabName);
   for (i = 0; i < show.length; i++) {
     show[i].style.display = "block";
-    if (tabName == "upload-form" && count > 1) {
-      displaySlider(count);
-    }
   }
 }
 
@@ -310,7 +269,7 @@ async function uploadPDF(event) {
         let listEl = document.createElement("li");
         listEl.id = ref;
         let pEl = document.createElement("p");
-        pEl.innerHTML = citationObj.entryHTMLString;
+        pEl.innerHTML = citationObj.entryHTMLString();
         if (citationObj.target) {
           let anchorEl = document.createElement("a");
           anchorEl.href = citationObj.target;
@@ -335,7 +294,7 @@ async function uploadPDF(event) {
         // }
 
         // Google scholar link
-        listEl.appendChild(citationObj.googleScholarAnchor);
+        listEl.appendChild(citationObj.googleScholarAnchor());
 
         oLinkEl.append(listEl);
       });
@@ -344,142 +303,10 @@ async function uploadPDF(event) {
 
   openTab("output-display", "tab-contents");
   $("output-main").scrollIntoView();
-  $("summary-link").style.display = "inline-block";
+  // $("summary-link").style.display = "inline-block";
 }
-
-class MLA8Citation {
-  constructor(citation) {
-    this.citation = citation;
-  }
-  /**
-   * @param {boolean} all - whether it should truncate at 2 authors.
-   * Default is False
-   * @returns {string} - concatenated author strings
-   */
-  joinAuthors(all = false) {
-    if (all) return this.authors.join(", ");
-    if (this.authors.length == 1) return this.authors[0];
-
-    var authorNames = "";
-    for (let index = 0; index < this.authors.length; index++) {
-      if (index == 2) {
-        authorNames += "et al.";
-        break;
-      }
-      authorNames += `${this.authors[index]}, `;
-    }
-
-    return authorNames;
-  }
-
-  get googleScholarAnchor() {
-    let rawDisplayName = this.joinAuthors();
-    let encodedQuery = encodeURI(
-      `${rawDisplayName} "${this.title}". ${this.journal} ${this.volume} ${this.date}`
-    );
-    let anchorEl = document.createElement("a");
-    anchorEl.href = `https://scholar.google.co.uk/scholar?q=${encodedQuery}`;
-    anchorEl.text = "Google Scholar";
-    anchorEl.target = "_blank";
-
-    return anchorEl;
-  }
-
-  // TODO: using string literal is too naive
-  get entryHTMLString() {
-    let displayName = this.joinAuthors(this.authors);
-    return `${displayName} ${this.title} <i>${this.journal}</i> ${this.date} ${this.pages}`;
-  }
-
-  get title() {
-    return this.citation.title;
-  }
-
-  get authors() {
-    let authors = [];
-    for (let index = 0; index < this.citation.authors.length; index++) {
-      const person_name = this.citation.authors[index].person_name;
-      let first_name = `${person_name.first_name} ` || "";
-      let surname = person_name.surname || "";
-      authors.push(`${first_name}${surname}`);
-    }
-    return authors;
-  }
-
-  // NOTE: add to MLA8 citation string
-  get publisher() {
-    return this.citation.publisher || "";
-  }
-
-  get journal() {
-    return this.citation.journal || "";
-  }
-
-  // NOTE: add to MLA8 citation string
-  get series() {
-    return this.citation.series || "";
-  }
-
-  get pages() {
-    let pages = this.citation.scope.pages;
-    if (pages === null) return "";
-
-    if (pages.from_page === pages.to_page) return `p. ${pages.to_page}.`;
-
-    return `pp. ${pages.from_page}-${pages.to_page}.`;
-  }
-
-  get volume() {
-    let volume = this.citation.scope.volume;
-    if (volume === null) return "";
-
-    return `vol. ${volume}`;
-  }
-
-  get date() {
-    let date = this.citation.date;
-    let year = date.year;
-    let month = date.month || "";
-    let day = date.day || "";
-
-    date = [year, month, day].join(" ").trim();
-    if (date !== "") {
-      date += ".";
-    }
-
-    return date;
-  }
-
-  get target() {
-    return this.citation.target || "";
-  }
-
-  get ids() {
-    let idUrlArr = [];
-    let citationIds = this.citation.ids;
-    if (citationIds.doi != null) {
-      idUrlArr.push({
-        id: citationIds.doi,
-        url: `https://doi.org/${citationIds.citation}`,
-      });
-    }
-    if (citationIds.arxiv != null) {
-      idUrlArr.push({
-        id: citationIds.arxiv,
-        url: `https://arxiv.org/abs/${citationIds.arxiv}`,
-      });
-    }
-
-    return idUrlArr;
-  }
-}
-
 
 /***********************************************FOR THE OUTPUT DISPLAY*******************************************************/
-
-var outputBoxes = document.querySelectorAll(".output-boxes");
-
-outputBoxes.forEach((box) => box.addEventListener("click", toggleOpen));
 
 /**
  * Toggles adding the open css class to a div.
---
+++ frontend/src/modules/MLA8Citation.js
@@ -0,0 +1,159 @@
+/** @module MLA8Citation */
+
+/**
+ * @typedef {object} idURL
+ * @property {string} id  - ID
+ * @property {string} url - URL based on ID
+ */
+
+/** @class */
+export default class MLA8Citation {
+  constructor(citation) {
+    this.citation = citation;
+  }
+  /**
+   * @param {boolean} all - whether it should truncate at 2 authors.
+   * Default is False
+   * @returns {string} - concatenated author strings
+   */
+  joinAuthors(all = false) {
+    if (all) return this.authors.join(", ");
+    if (this.authors.length == 1) return this.authors[0];
+
+    var authorNames = "";
+    for (let index = 0; index < this.authors.length; index++) {
+      if (index == 2) {
+        authorNames += "et al.";
+        break;
+      }
+      authorNames += `${this.authors[index]}, `;
+    }
+
+    return authorNames;
+  }
+
+  /**
+   * @returns {HTMLAnchorElement} Google scholar query link
+   */
+  googleScholarAnchor() {
+    let rawDisplayName = this.joinAuthors();
+    let encodedQuery = encodeURI(
+      `${rawDisplayName} "${this.title}". ${this.journal} ${this.volume} ${this.date}`
+    );
+    let anchorEl = document.createElement("a");
+    anchorEl.href = `https://scholar.google.co.uk/scholar?q=${encodedQuery}`;
+    anchorEl.text = "Google Scholar";
+    anchorEl.target = "_blank";
+
+    return anchorEl;
+  }
+
+  // TODO: using string literal is too naive
+  /**
+   * @returns {string} HTML string structured as an MLA8 Citation entry
+   */
+  entryHTMLString() {
+    let displayName = this.joinAuthors(this.authors);
+    return `${displayName} ${this.title} <i>${this.journal}</i> ${this.date} ${this.pages}`;
+  }
+
+  /** @returns {string} citation title */
+  get title() {
+    return this.citation.title;
+  }
+
+  /** @returns {Array<string>} authors full names */
+  get authors() {
+    let authors = [];
+    for (let index = 0; index < this.citation.authors.length; index++) {
+      const person_name = this.citation.authors[index].person_name;
+      let first_name = `${person_name.first_name} ` || "";
+      let surname = person_name.surname || "";
+      authors.push(`${first_name}${surname}`);
+    }
+    return authors;
+  }
+
+  // NOTE: add to MLA8 citation string
+  /** @returns  {string} citation publisher (can be empty string) */
+  get publisher() {
+    return this.citation.publisher || "";
+  }
+
+  /** @returns  {string} citation journal (can be empty string) */
+  get journal() {
+    return this.citation.journal || "";
+  }
+
+  // NOTE: add to MLA8 citation string
+  /** @returns  {string} citation series (can be empty string) */
+  get series() {
+    return this.citation.series || "";
+  }
+
+  /**
+   * Format depends on if there is a range of pages
+   *
+   * @returns {string} formatted string of pages (can be empty)
+   */
+  get pages() {
+    let pages = this.citation.scope.pages;
+    if (pages === null) return "";
+
+    if (pages.from_page === pages.to_page) return `p. ${pages.to_page}.`;
+
+    return `pp. ${pages.from_page}-${pages.to_page}.`;
+  }
+
+  /** @returns  {string} formatted citation volume (can be empty string) */
+  get volume() {
+    let volume = this.citation.scope.volume;
+    if (volume === null) return "";
+
+    return `vol. ${volume}`;
+  }
+
+  /** @returns  {string} formatted citation date (can be empty string) */
+  get date() {
+    let date = this.citation.date;
+    let year = date.year;
+    let month = date.month || "";
+    let day = date.day || "";
+
+    date = [year, month, day].join(" ").trim();
+    if (date !== "") {
+      date += ".";
+    }
+
+    return date;
+  }
+
+  /** @returns  {string} citation target (can be empty string) */
+  get target() {
+    return this.citation.target || "";
+  }
+
+  /**
+   * Supported types: DOI and arXiv
+   *
+   * @returns {idURL[]} - array of {idURL}
+   */
+  get ids() {
+    let idUrlArr = [];
+    let citationIds = this.citation.ids;
+    if (citationIds.doi != null) {
+      idUrlArr.push({
+        id: citationIds.doi,
+        url: `https://doi.org/${citationIds.citation}`,
+      });
+    }
+    if (citationIds.arxiv != null) {
+      idUrlArr.push({
+        id: citationIds.arxiv,
+        url: `https://arxiv.org/abs/${citationIds.arxiv}`,
+      });
+    }
+
+    return idUrlArr;
+  }
+}
