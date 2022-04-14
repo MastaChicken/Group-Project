c5ad796c1baae5468f12efa1c8b710ff169a5e8a
William Michiel Simson van Dijkhuizen <psyws2@nottingham.ac.uk>
2022-04-14 18:53:38 +0200

    Add separate screens for upload and display

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,75 +3,144 @@ import { dropHandler, dragOverHandler } from "./modules/drag_drop";
 import { isValidPDF } from "./modules/pdf";
 import { $ } from "./constants";
 
-// Adds event listener to the upload button, so it executes on change.
-$("pdfpicker-file").addEventListener(
-  "change",
-  () => {
-    let files = $("pdfpicker-file").files;
-    let dropText = $("drop-text");
-    // checks file exists and passes PDF checks.
+import Upload from "./views/Upload.js";
+import Display from "./views/Display.js";
+import Settings from "./views/Settings.js";
 
-    if (files.length > 0 && isValidPDF(files[0])) {
-      dropText.innerHTML = `File accepted: ${files[0].name}`;
-      $("selection-boxes").style.display = "block";
-    } else {
-      // throws alert for wrong file type
-      $("pdfpicker-file").value = "";
-      dropText.innerHTML = "File Rejected: Please add .pdf file type";
-      $("selection-boxes").style.display = "none";
+const pathToRegex = (path) =>
+  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = (match) => {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1]
+  );
+
+  return Object.fromEntries(
+    keys.map((key, i) => {
+      return [key, values[i]];
+    })
+  );
+};
+
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = async () => {
+  const routes = [
+    { path: "/", view: Upload },
+    { path: "/display", view: Display },
+    { path: "/settings", view: Settings },
+  ];
+
+  // Test each route for potential match
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      result: location.pathname.match(pathToRegex(route.path)),
+    };
+  });
+
+  let match = potentialMatches.find(
+    (potentialMatch) => potentialMatch.result !== null
+  );
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      result: [location.pathname],
+    };
+  }
+
+  const view = new match.route.view(getParams(match));
+
+  document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
     }
-  },
-  false
-);
+  });
 
-let sos = $("size-of-summary");
-sos.addEventListener(
-  "change",
-  () => {
-    $("sos-lbl").innerHTML = `Size of Summary: ${sos.value}%`;
-  },
-  true
-);
+  router();
+});
 
-// Upload PDF form
-let uploadForm = $("upload-form");
-uploadForm.addEventListener("submit", (ev) => uploadPDF(ev));
+// // Adds event listener to the upload button, so it executes on change.
+// $("pdfpicker-file").addEventListener(
+//   "change",
+//   () => {
+//     let files = $("pdfpicker-file").files;
+//     let dropText = $("drop-text");
+//     // checks file exists and passes PDF checks.
 
-// Upload URL
-let urlInput = $("url-input");
-urlInput.addEventListener("click", validateURL);
+//     if (files.length > 0 && isValidPDF(files[0])) {
+//       dropText.innerHTML = `File accepted: ${files[0].name}`;
+//       $("selection-boxes").style.display = "block";
+//     } else {
+//       // throws alert for wrong file type
+//       $("pdfpicker-file").value = "";
+//       dropText.innerHTML = "File Rejected: Please add .pdf file type";
+//       $("selection-boxes").style.display = "none";
+//     }
+//   },
+//   false
+// );
 
-// Drophandler
-let dropZone = $("drop-zone");
-dropZone.addEventListener("drop", (ev) => dropHandler(ev));
-dropZone.addEventListener("dragover", (ev) => dragOverHandler(ev));
+// let sos = $("size-of-summary");
+// sos.addEventListener(
+//   "change",
+//   () => {
+//     $("sos-lbl").innerHTML = `Size of Summary: ${sos.value}%`;
+//   },
+//   true
+// );
 
-// Toggle PDF
-let pdfToggleInput = $("output-show-document");
-// TODO: use event to check if its toggled or not
-pdfToggleInput.addEventListener("change", togglePDFDisplay);
+// // Upload PDF form
+// let uploadForm = $("upload-form");
+// uploadForm.addEventListener("submit", (ev) => uploadPDF(ev));
 
-// Output display
-let outputBoxes = document.querySelectorAll(".output-boxes");
+// // Upload URL
+// let urlInput = $("url-input");
+// urlInput.addEventListener("click", validateURL);
 
-outputBoxes.forEach((box) => box.addEventListener("click", toggleOpen));
+// // Drophandler
+// let dropZone = $("drop-zone");
+// dropZone.addEventListener("drop", (ev) => dropHandler(ev));
+// dropZone.addEventListener("dragover", (ev) => dragOverHandler(ev));
 
-/***********************************************FOR THE OUTPUT DISPLAY*******************************************************/
+// // Toggle PDF
+// let pdfToggleInput = $("output-show-document");
+// // TODO: use event to check if its toggled or not
+// pdfToggleInput.addEventListener("change", togglePDFDisplay);
 
-/**
- * Toggles adding the open css class to a div.
- */
-function toggleOpen() {
-  this.classList.toggle("open");
-}
+// // Output display
+// let outputBoxes = document.querySelectorAll(".output-boxes");
 
-/**
- * Toggle whether the pdf renderer is being displayed or not. If it is not displayed then then changes grid template to
- * one column as opposed to 2 and vice versa.
- */
-function togglePDFDisplay() {
-  $("pdf-renderer").style.display =
-    $("pdf-renderer").style.display == "none" ? "block" : "none";
-  $("output-main").style.gridTemplateColumns =
-    $("output-main").style.gridTemplateColumns == "1fr" ? "1fr 1fr" : "1fr";
-}
+// outputBoxes.forEach((box) => box.addEventListener("click", toggleOpen));
+
+// /***********************************************FOR THE OUTPUT DISPLAY*******************************************************/
+
+// /**
+//  * Toggles adding the open css class to a div.
+//  */
+// function toggleOpen() {
+//   this.classList.toggle("open");
+// }
+
+// /**
+//  * Toggle whether the pdf renderer is being displayed or not. If it is not displayed then then changes grid template to
+//  * one column as opposed to 2 and vice versa.
+//  */
+// function togglePDFDisplay() {
+//   $("pdf-renderer").style.display =
+//     $("pdf-renderer").style.display == "none" ? "block" : "none";
+//   $("output-main").style.gridTemplateColumns =
+//     $("output-main").style.gridTemplateColumns == "1fr" ? "1fr 1fr" : "1fr";
+// }
