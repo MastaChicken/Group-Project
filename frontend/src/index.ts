5984d483ed9879a0326b4828b541fc928d01bf6a
Omar Zeghouani <psyoz1@nottingham.ac.uk>
2022-04-18 23:43:52 +0100

    Update documentation for shoelace

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,7 +1,6 @@
 import "./routes";
-import '@shoelace-style/shoelace/dist/themes/light.css';
-import '@shoelace-style/shoelace/dist/components/button/button.js';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 
 // Set the base path to the folder you copied Shoelace's assets to
-setBasePath('/dist/shoelace');
+setBasePath("/dist/shoelace");
