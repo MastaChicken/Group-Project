e37445b6e252f52a453ca8d09d7757684c6d9d4f
Omar Zeghouani <psyoz1@nottingham.ac.uk>
2022-04-22 22:18:07 +0100

    Make playwright launch web server

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,10 +1,14 @@
-// playwright.config.js
-import { devices } from "@playwright/test";
+import { devices, PlaywrightTestConfig } from "@playwright/test";
 
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
+const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  webServer: {
+    command: "npm run dev",
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     trace: "on-first-retry",
   },
