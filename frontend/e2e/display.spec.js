77b98bb7aabf315ace15cca674e6d4a05deba8bd
William Michiel Simson van Dijkhuizen <psyws2@nottingham.ac.uk>
2022-04-24 01:42:27 +0200

    Merge related tests

--- frontend/e2e/display.spec.js
+++ frontend/e2e/display.spec.js
@@ -1,5 +1,4 @@
 import { test, expect } from "@playwright/test";
-import { atRule } from "postcss";
 
 /**
  * Test the display screen.
@@ -10,37 +9,21 @@ test("Checks if the page is display", async ({ page }) => {
 });
 
 /**
- * Test the checkbox for display PDF when.
+ * Test to see if renderer is visible when checkbox is ticked.
  */
-test("Check if show document checkbox can be enabled", async ({ page }) => {
+test("Check if pdf renderer is visible", async ({ page }) => {
   await page.goto("/display");
   await page.check("#output-show-document");
   expect(await page.isChecked("#output-show-document")).toBeTruthy();
-});
-
-/**
- * Test the checkbox for display PDF when disabled.
- */
-test("Check if show document checkbox can be disabled", async ({ page }) => {
-  await page.goto("/display");
-  await page.uncheck("#output-show-document");
-  expect(await page.isChecked("#output-show-document")).toBeFalsy();
-});
-
-/**
- * Test to see if renderer is visible when checkbox is ticked.
- */
-test("Check visibility of pdf render", async ({ page }) => {
-  await page.goto("/display");
-  await page.check("#output-show-document");
   expect(await page.isVisible("#pdf-renderer")).toBeTruthy();
 });
 
 /**
  * Test to see if renderer is invisible when checkbox is unticked.
  */
-test("Check if pdf render is invisible", async ({ page }) => {
+test("Check if pdf renderer is invisible", async ({ page }) => {
   await page.goto("/display");
   await page.uncheck("#output-show-document");
+  expect(await page.isChecked("#output-show-document")).toBeFalsy();
   expect(await page.isHidden("#pdf-renderer")).toBeTruthy();
 });
