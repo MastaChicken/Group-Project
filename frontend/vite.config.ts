268e27e8034747cd466c4aaec07316ac67e5b750
Omar Zeghouani <psyoz1@nottingham.ac.uk>
2022-04-26 20:34:29 +0100

    Move assets to public folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import checker from "vite-plugin-checker";
 import EnvironmentPlugin from "vite-plugin-environment";
 import template from "rollup-plugin-html-literals";
@@ -19,14 +18,6 @@ export default defineConfig({
       // Required: will fail if the API_URL environment variable is not provided.
       API_URL: undefined,
     }),
-      viteStaticCopy({
-        targets: [
-          {
-            src: resolve(__dirname, "assets"),
-            dest: resolve(__dirname, "dist"),
-          },
-        ],
-      }),
   ],
   build: {
     minify: true,
