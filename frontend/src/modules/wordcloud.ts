1fa098f38b1db225cbfc7bc60d614ce5ab0b0ccc
Omar Zeghouani <psyoz1@nottingham.ac.uk>
2022-04-29 23:52:46 +0100

    Add TSDoc to wordcloud module

--- frontend/src/modules/wordcloud.ts
+++ frontend/src/modules/wordcloud.ts
@@ -1,15 +1,21 @@
 import wordcloud from "wordcloud";
 
+/**
+ * Represents the wordcloud using a canvas
+ *
+ */
 export default class WordCloudCanvas {
   rect: DOMRect;
   constructor(container: HTMLDivElement) {
     this.rect = container.getBoundingClientRect();
   }
 
+  /** Returns width relative to container */
   public get width(): number {
     return this.rect.width * 0.8;
   }
 
+  /** Returns height relative to width */
   public get height(): number {
     return this.width * 0.65;
   }
