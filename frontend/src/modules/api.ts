bfcbc951f30f8610a88717c12ef4d3186c1e0eb2
William Michiel Simson van Dijkhuizen <psyws2@nottingham.ac.uk>
2022-05-01 00:01:17 +0200

    Refactor by separating svg functionality from api.ts

--- frontend/src/modules/api.ts
+++ frontend/src/modules/api.ts
@@ -5,6 +5,7 @@ import { UploadResponse, Author, Citation, CitationIDs } from "../models/api";
 import { renderPDF } from "./PDFRenderer";
 import { createAlert, Icon, Variant } from "./alert";
 import { SlDialog } from "@shoelace-style/shoelace";
+import { prepareSVGMaker } from "./svg";
 
 /**
  * Resets form and sets the text to default state.
@@ -252,59 +253,6 @@ export async function uploadPDF(file: File) {
     .catch((e) => {
       displayError(e, uploadCodesMap, "Server is down. Please try again later");
     });
-}
-
-function prepareSVGMaker(logos: HTMLElement, citation: Citation) {
-  if (citation.ids == null) {
-    return;
-  }
-  console.log(citation.ids.DOI);
-  console.log(citation.ids.arXiv);
-  let target = citation.target;
-
-  // Check if last character is a / because if it is then link is incomplete.
-  if (target != null) {
-    if (target.charAt(target.length - 1) == "/") {
-      target = null;
-    }
-  }
-  const id: CitationIDs = citation.ids;
-
-  if (id.DOI) {
-    if (target == null) {
-      target = `https://doi.org/${id.DOI}`;
-    } else {
-      target = citation.target;
-    }
-  } else if (id.arXiv) {
-    if (target == null) {
-      target = `https://arxiv.org/abs/${id.arXiv}`;
-    } else {
-      target = citation.target;
-    }
-  }
-  makeSVG(logos, citation.ids, target);
-}
-
-function makeSVG(logos: HTMLElement, id: CitationIDs, target: string) {
-  const anchorEl = document.createElement("a");
-
-  anchorEl.href = target;
-  anchorEl.target = "_blank";
-  const img = document.createElement("img");
-
-  if (id.DOI) {
-    img.src = "../../public/assets/icons/DOI_logo.svg";
-    img.alt = "DOI";
-  } else if (id.arXiv) {
-    img.src = "../../public/assets/icons/ArXiv_web.svg";
-    img.alt = "arXiv";
-  }
-
-  img.width = 45;
-  img.height = 45;
-  anchorEl.append(img);
-  logos.append(anchorEl);
 }
 
 /**
